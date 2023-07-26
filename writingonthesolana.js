feb301248c3d76b3df2243394fd79b7e49fe4206
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-26 19:11:37 +0530

    deserialaztion code created

--- writingonthesolana.js
+++ writingonthesolana.js
@@ -1,46 +1,4 @@
-// const {
-//   Connection,
-//   clusterApiUrl,
-//   Keypair,
-//   Transaction,
-//   SystemProgram,
-//   PublicKey,
-//   LAMPORTS_PER_SOL,
-//   sendAndConfirmTransaction,
-// } = require("@solana/web3.js");
 
-// const fs = require("fs");
-
-// async function main(){
-//   const connection = new Connection(clusterApiUrl("devnet"));
-//   const secert = JSON.parse(fs.readFileSync("secretkey.json") || "[]");
-//   const secertkey = new Uint8Array(secert);
-
-//   //getting the public key
-//   const ownerKeypair = Keypair.fromSecretKey(secertkey);
-
-//   const publicKey = ownerKeypair.publicKey;
-//   console.log("public key ", publicKey);
-//   const recipientAddress = "FbcBWzcezM77bzx7CxK7EgtGk6VBTeqBPBwJkd17amov";
-//   const recipient = new PublicKey(recipientAddress);
-
-//   const transaction = new Transaction();
-
-//   const sendSolInstruction = SystemProgram.transfer({
-//     fromPubkey: publicKey,
-//     toPubkey: recipient,
-//     lamports: LAMPORTS_PER_SOL * 0.1,
-//   });
-
-//   transaction.add(sendSolInstruction);
-
-//   const signature = await sendAndConfirmTransaction(connection, transaction, [
-//     ownerKeypair,
-//   ]);
-//   console.log(signature, "signature");
-// };
-
-// main()
 const {
   clusterApiUrl,
   Connection,
